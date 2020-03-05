69def54a769db3a40b374460f2cd3c8276c1817b
EC2 Default User <ec2-user@ip-172-31-27-92.ap-southeast-1.compute.internal>
2020-03-05 13:44:41 +0000

    レビューを表示

---
+++ reviewApp.py
@@ -0,0 +1,11 @@
+# 変数の定義
+genre = "映画"
+title = "時をかける少女"
+review = "人生の最高傑作アニメ。\n\n青春厨としてはたまらない甘酸っぱい青春ストーリー。\n\nちあきのイケメンさは言うまでもない。\n\n人にお金を払ってでもみて欲しい作品。\n\n早く見たほうがいいよ。\n\nTime waits for no one."
+line = "\n---------------------------"
+
+# レビューを表示
+print("ジャンル : " + genre + line)
+print("タイトル : " + title + line)
+print("感想 : ")
+print(review + line)
