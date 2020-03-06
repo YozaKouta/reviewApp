8c59c0bdd58fd9e9c02d95618b39faf1a0bf76a9
EC2 Default User <ec2-user@ip-172-31-27-92.ap-southeast-1.compute.internal>
2020-03-06 13:57:34 +0000

    レビューの内容をターミナルから入力

--- reviewApp.py
+++ reviewApp.py
@@ -1,11 +1,17 @@
 # 変数の定義
-genre = "映画"
-title = "時をかける少女"
-review = "人生の最高傑作アニメ。\n\n青春厨としてはたまらない甘酸っぱい青春ストーリー。\n\nちあきのイケメンさは言うまでもない。\n\n人にお金を払ってでもみて欲しい作品。\n\n早く見たほうがいいよ。\n\nTime waits for no one."
+# genre = "映画"
+# title = "時をかける少女"
+# review = "人生の最高傑作アニメ。\n\n青春厨としてはたまらない甘酸っぱい青春ストーリー。\n\nちあきのイケメンさは言うまでもない。\n\n人にお金を払ってでもみて欲しい作品。\n\n早く見たほうがいいよ。\n\nTime waits for no one."
+
+print("ジャンルを入力してください：")
+genre = input()
+print("タイトルを入力してください：")
+title = input()
+print("感想を入力してください：")
+review = input()
 line = "\n---------------------------"
 
 # レビューを表示
-print("ジャンル : " + genre + line)
+print("\nジャンル : " + genre + line)
 print("タイトル : " + title + line)
-print("感想 : ")
-print(review + line)
+print("感想 : \n" + review + line)
