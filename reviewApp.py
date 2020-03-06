b807cdfeb4d54cd958083a617db0e859971c376a
EC2 Default User <ec2-user@ip-172-31-27-92.ap-southeast-1.compute.internal>
2020-03-06 14:06:00 +0000

    辞書オブジェクトを使って情報を整理

--- reviewApp.py
+++ reviewApp.py
@@ -1,17 +1,19 @@
 # 変数の定義
+post = {} #空の辞書の宣言
+
 # genre = "映画"
 # title = "時をかける少女"
 # review = "人生の最高傑作アニメ。\n\n青春厨としてはたまらない甘酸っぱい青春ストーリー。\n\nちあきのイケメンさは言うまでもない。\n\n人にお金を払ってでもみて欲しい作品。\n\n早く見たほうがいいよ。\n\nTime waits for no one."
 
 print("ジャンルを入力してください：")
-genre = input()
+post["genre"] = input()
 print("タイトルを入力してください：")
-title = input()
+post["title"] = input()
 print("感想を入力してください：")
-review = input()
+post["review"] = input()
 line = "\n---------------------------"
 
 # レビューを表示
-print("\nジャンル : " + genre + line)
-print("タイトル : " + title + line)
-print("感想 : \n" + review + line)
+print("\nジャンル : " + post["genre"] + line)
+print("タイトル : " + post["title"] + line)
+print("感想 : \n" + post["review"] + line)
